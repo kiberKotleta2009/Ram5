03b4590460edf31f24b70af7cb4c41714a98d89b
kiberKotleta2009 <60068663+kiberKotleta2009@users.noreply.github.com>
2020-12-12 10:20:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def menu():
     s = "<ul>"
-    s += '<li><p><a href="task1/random">task1/random</a></p></li>'
-    s += '<li><p><a href="task1/i_will_not">task1/i_will_not</a></p></li>'
+    s += '<li><p><a href="task1/random">task1/random/</a></p></li>'
+    s += '<li><p><a href="task1/i_will_not">task1/i_will_not/</a></p></li>'
     s += "</ul>"
     out = "<pre id=menu>{}</pre>".format(s)
     return out
 
 
-@app.route('/haba')
+@app.route('/haba/')
 def hello_world():
     s = ["Hello, Haba!",
          "Hello, Arsen!",
@@ -24,14 +24,14 @@ def hello_world():
     return out
 
 
-@app.route('/task1/random')
+@app.route('/task1/random/')
 def task1():
     a = randint(1, 3)
     out = "<pre>{}</pre>".format("Haba's mark is " + str(a))
     return out
 
 
-@app.route('/task1/i_will_not')
+@app.route('/task1/i_will_not/')
 def task1_never():
     s = "<ul>"
     for i in range(100):
